017b9dbd78e581f3257b8575ed0d869ee592a381
LateOne15 <48002046+LateOne15@users.noreply.github.com>
2025-09-04 14:59:45 +0800

    0.1.1 btnEditWIP
    WIP on btnEdit

--- AstronomicalProcessing/Astronomical/MainWindow.xaml.cs
+++ AstronomicalProcessing/Astronomical/MainWindow.xaml.cs
@@ -19,6 +19,49 @@ namespace Astronomical
         public MainWindow()
         {
             InitializeComponent();
+            int[] ints = new int[24];
+            Random rnd = new Random();
+            for (int i = 0; i < 24; i++)
+            {
+                ints[i] = rnd.Next(10,91);
+            }
+            foreach(int i in ints)
+            {
+                lbxDataList.Items.Add(i);
+            }
+        }
+
+        #region Messages
+        private void DisplayMessage(string msg, string caption)
+        {
+            MessageBox.Show(msg, caption, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void DisplayError(string msg, string caption)
+        {
+            MessageBox.Show(msg, caption, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        //private void DisplayStatus(string msg)
+        //{
+        //    txtStatus.Text = msg;
+        //}
+        #endregion
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSort_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            lbxDataList.SelectedItem = tbEdit.Text;
+            tbEdit.Text= string.Empty;
         }
     }
 }
